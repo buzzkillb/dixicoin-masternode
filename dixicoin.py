5b73917be40a9d0a621a6c76b4815900977c4e09
buzzkillb <travanx@gmail.com>
2018-04-25 23:11:25 -0700

    Create dixicoin.py

---
+++ dixicoin.py
@@ -0,0 +1,328 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from subprocess import Popen,PIPE,STDOUT
+import collections
+import os
+import sys
+import time
+import math
+import os
+from urllib2 import urlopen
+
+SERVER_IP = urlopen('http://ip.42.pl/raw').read()
+NODE_LIST = urlopen('https://pastebin.com/raw/YBCuR2Qt').read()
+#BOOTSTRAP_URL = "https://gitlab.com/denarius/chaindata/raw/master/chaindata.zip"
+
+DEFAULT_COLOR = "\x1b[0m"
+PRIVATE_KEYS = []
+
+def print_info(message):
+    BLUE = '\033[94m'
+    print(BLUE + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def print_warning(message):
+    YELLOW = '\033[93m'
+    print(YELLOW + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def print_error(message):
+    RED = '\033[91m'
+    print(RED + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def get_terminal_size():
+    import fcntl, termios, struct
+    h, w, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return w, h
+    
+def remove_lines(lines):
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    for l in lines:
+        sys.stdout.write(CURSOR_UP_ONE + '\r' + ERASE_LINE)
+        sys.stdout.flush()
+
+def run_command(command):
+    out = Popen(command, stderr=STDOUT, stdout=PIPE, shell=True)
+    lines = []
+    
+    while True:
+        line = out.stdout.readline()
+        if (line == ""):
+            break
+        
+        # remove previous lines     
+        remove_lines(lines)
+        
+        w, h = get_terminal_size()
+        lines.append(line.strip().encode('string_escape')[:w-3] + "\n")
+        if(len(lines) >= 5):
+            del lines[0]
+
+        # print lines again
+        for l in lines:
+            sys.stdout.write('\r')
+            sys.stdout.write(l)
+        sys.stdout.flush()
+
+    remove_lines(lines) 
+    out.wait()
+
+
+def print_welcome():
+    os.system('clear')
+
+    print("          _____                                            _____          
+    print("         /\    \                 ______                   /\    \         
+    print("        /::\    \               |::|   |                 /::\    \        
+    print("       /::::\    \              |::|   |                /::::\    \       
+    print("      /::::::\    \             |::|   |               /::::::\    \      
+    print("     /:::/\:::\    \            |::|   |              /:::/\:::\    \     
+    print("    /:::/  \:::\    \           |::|   |             /:::/  \:::\    \    
+    print("   /:::/    \:::\    \          |::|   |            /:::/    \:::\    \   
+    print("  /:::/    / \:::\    \         |::|   |           /:::/    / \:::\    \  
+    print(" /:::/    /   \:::\ ___\  ______|::|___|___ ____  /:::/    /   \:::\    \ 
+    print("/:::/____/     \:::|    ||:::::::::::::::::|    |/:::/____/     \:::\____\
+    print("\:::\    \     /:::|____||:::::::::::::::::|____|\:::\    \      \::/    /
+    print(" \:::\    \   /:::/    /  ~~~~~~|::|~~~|~~~       \:::\    \      \/____/ 
+    print("  \:::\    \ /:::/    /         |::|   |           \:::\    \             
+    print("   \:::\    /:::/    /          |::|   |            \:::\    \            
+    print("    \:::\  /:::/    /           |::|   |             \:::\    \           
+    print("     \:::\/:::/    /            |::|   |              \:::\    \          
+    print("      \::::::/    /             |::|   |               \:::\    \         
+    print("       \::::/    /              |::|   |                \:::\____\        
+    print("        \::/____/               |::|___|                 \::/    /        
+    print("         ~~                      ~~                       \/____/         
+    print("")
+    print_info("Dixicoin masternode(s) installer v1.337 nocompile")
+    print("")
+    print("")
+    print("")
+
+def update_system():
+    print_info("Updating the system...")
+    run_command("apt-get update")
+    # special install for grub
+    run_command('sudo DEBIAN_FRONTEND=noninteractive apt-get -yq -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc')
+    run_command("apt-get upgrade -y")
+
+def chech_root():
+    print_info("Check root privileges")
+    user = os.getuid()
+    if user != 0:
+        print_error("This program requires root privileges.  Run as root user.")
+        sys.exit(-1)
+
+# def secure_server():
+#    print_info("Securing server...")
+#    run_command("apt-get --assume-yes install ufw")
+#    run_command("ufw allow ssh/tcp")
+#    run_command("ufw limit ssh/tcp")
+#    run_command("ufw allow 33339/tcp")
+#    run_command("ufw allow 9999/tcp")
+#    run_command("ufw --force enable")
+
+def compile_wallet():
+    print_info("Allocating swap...")
+    run_command("fallocate -l 3G /swapfile")
+    run_command("chmod 600 /swapfile")
+    run_command("mkswap /swapfile")
+    run_command("swapon /swapfile")
+    f = open('/etc/fstab','r+b')
+    line = '/swapfile   none    swap    sw    0   0 \n'
+    lines = f.readlines()
+    if (lines[-1] != line):
+        f.write(line)
+        f.close()
+
+    print_info("Installing wallet build dependencies...")
+    run_command("apt-get --assume-yes install git unzip build-essential libssl-dev libdb++-dev libboost-all-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev libgmp-dev libgmp3-dev autoconf autogen automake  libtool")
+    run_command("add-apt-repository ppa:bitcoin/bitcoin -y")
+    run_command("apt-get update")
+    run_command("apt-get --assume-yes install git unzip libdb4.8-dev libdb4.8++-dev")
+    # is_compile = True
+    # if os.path.isfile('/usr/local/bin/denariusd'):
+    #    print_warning('Wallet already installed on the system')
+    #    is_compile = False
+
+    # if is_compile:
+    #    print_info("Downloading wallet...")
+    #    run_command("rm -rf /opt/Denarius")
+    #    run_command("git clone https://github.com/carsenk/denarius /opt/Denarius")
+    #    
+    #    print_info("Compiling wallet...")
+    #    run_command("chmod +x /opt/Denarius/src/leveldb/build_detect_platform")
+    #    run_command("chmod +x /opt/Denarius/src/secp256k1/autogen.sh")
+    #    run_command("cd  /opt/Denarius/src/ && make -f makefile.unix USE_UPNP=-")
+    #    run_command("strip /opt/Denarius/src/denariusd")
+    #    run_command("cp /opt/Denarius/src/denariusd /usr/local/bin")
+    #    run_command("cd /opt/Denarius/src/ &&  make -f makefile.unix clean")
+    #    run_command("denariusd")
+		
+    print_info("Downloading wallet...")
+    run_command("wget https://github.com/Dixicoin-DXC/Dixicoin/releases/download/v4.2/dixi-4.2.0-aarch64-linux-gnu.zip")
+    run_command("unzip dixi-4.2.0-aarch64-linux-gnu.zip -d dixicoin")
+    run_command("mv dixicoin/dixi-4.2.0-aarch64-linux-gnu/dixicoind /usr/local/bin/dixicoind")
+    run_command("rm dixi-4.2.0-aarch64-linux-gnu.zip")		
+
+def get_total_memory():
+    return (os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES'))/(1024*1024)
+
+def autostart_masternode(user):
+    job = "@reboot /usr/local/bin/dixicoind\n"
+    
+    p = Popen("crontab -l -u {} 2> /dev/null".format(user), stderr=STDOUT, stdout=PIPE, shell=True)
+    p.wait()
+    lines = p.stdout.readlines()
+    if job not in lines:
+        print_info("Cron job doesn't exist yet, adding it to crontab")
+        lines.append(job)
+        p = Popen('echo "{}" | crontab -u {} -'.format(''.join(lines), user), stderr=STDOUT, stdout=PIPE, shell=True)
+        p.wait()
+
+def setup_first_masternode():
+    print_info("Setting up first masternode")
+    run_command("useradd --create-home -G sudo mn1")
+    os.system('su - mn1 -c "{}" '.format("dixicoind -daemon &> /dev/null"))
+
+    print_info("Open your desktop wallet config file (%appdata%/Dixicoin/dixicoin.conf) and copy your rpc username and password! If it is not there create one! E.g.:\n\trpcuser=[SomeUserName]\n\trpcpassword=[DifficultAndLongPassword]")
+    global rpc_username
+    global rpc_password
+    rpc_username = raw_input("rpcuser: ")
+    rpc_password = raw_input("rpcpassword: ")
+
+    print_info("Open your wallet console (Help => Debug window => Console) and create a new masternode private key: masternode genkey")
+    masternode_priv_key = raw_input("masternodeprivkey: ")
+    PRIVATE_KEYS.append(masternode_priv_key)
+    
+    config = """rpcuser={}
+rpcpassword={}
+rpcallowip=127.0.0.1
+rpcport=61151
+port=61150
+server=1
+listen=1
+daemon=1
+logtimestamps=1
+mnconflock=1
+masternode=1
+masternodeaddr={}:61150
+masternodeprivkey={}
+{}""".format(rpc_username, rpc_password, SERVER_IP, masternode_priv_key, NODE_LIST)
+
+    print_info("Saving config file...")
+    run_command('su - mn1 -c "{}" '.format("mkdir -p /home/mn1/.dixicoin/ && touch /home/mn1/.dixicoin/dixicoin.conf"))
+    f = open('/home/mn1/.dixicoin/dixicoin.conf', 'w')
+    f.write(config)
+    f.close()
+
+#    print_info("Downloading chaindata.zip file...")
+#    run_command('su - mn1 -c "{}" '.format("cd && wget --continue " + BOOTSTRAP_URL))
+    
+#    print_info("Unzipping chaindata.zip...")
+#    filename = BOOTSTRAP_URL[BOOTSTRAP_URL.rfind('/')+1:]
+#    run_command('su - mn1 -c "{}" '.format("cd && unzip -d .dixicoin -o " + filename))
+
+    run_command('rm /home/mn1/.dixicoin/peers.dat') 
+    autostart_masternode('mn1')
+    os.system('su - mn1 -c "{}" '.format('dixicoind -daemon &> /dev/null'))
+    print_warning("Masternode started syncing in the background...")
+
+def setup_xth_masternode(xth):
+    print_info("Setting up {}th masternode".format(xth))
+    run_command("useradd --create-home -G sudo mn{}".format(xth))
+    run_command("rm -rf /home/mn{}/.denarius/".format(xth))
+
+    print_info('Copying wallet data from the first masternode...')
+    run_command("cp -rf /home/mn1/.denarius /home/mn{}/".format(xth))
+    run_command("sudo chown -R mn{}:mn{} /home/mn{}/.denarius".format(xth, xth, xth))
+    run_command("rm /home/mn{}/.denarius/peers.dat &> /dev/null".format(xth))
+    run_command("rm /home/mn{}/.denarius/wallet.dat &> /dev/null".format(xth))
+
+    print_info("Open your wallet console (Help => Debug window => Console) and create a new masternode private key: masternode genkey")
+    masternode_priv_key = raw_input("masternodeprivkey: ")
+    PRIVATE_KEYS.append(masternode_priv_key)
+
+    BASE_RPC_PORT = 32339
+    BASE_PORT = 9999
+    
+    config = """rpcuser={}
+rpcpassword={}
+rpcallowip=127.0.0.1
+rpcport={}
+port={}
+server=1
+listen=1
+daemon=1
+logtimestamps=1
+mnconflock=1
+masternode=1
+masternodeaddr={}:{}
+masternodeprivkey={}
+{}""".format(rpc_username, rpc_password, BASE_RPC_PORT + xth - 1, BASE_PORT + xth - 1, SERVER_IP, BASE_PORT + xth - 1, masternode_priv_key, NODE_LIST)
+    
+    print_info("Saving config file...")
+    f = open('/home/mn{}/.denarius/denarius.conf'.format(xth), 'w')
+    f.write(config)
+    f.close()
+    
+    autostart_masternode('mn'+str(xth))
+    os.system('su - mn{} -c "{}" '.format(xth, 'denariusd  -daemon &> /dev/null'))
+    print_warning("Masternode started syncing in the background...")
+    
+
+def setup_masternodes():
+    memory = get_total_memory()
+    masternodes = int(math.floor(memory / 300))
+    print_info("This system is capable to run around {} masternodes. To support Denarius network only use one masternode per ip.".format(masternodes))
+    print_info("How much masternodes do you want to setup?")
+    masternodes = int(raw_input("Number of masternodes: "))
+   
+    if masternodes >= 1:
+        setup_first_masternode()
+
+    for i in range(masternodes-1):
+        setup_xth_masternode(i+2)
+
+def porologe():
+
+    mn_base_data = """
+Alias: Masternode{}
+IP: {}
+Private key: {}
+Transaction ID: [1k desposit transaction id. 'masternode outputs']
+Transaction index: [1k desposit transaction index. 'masternode outputs']
+--------------------------------------------------
+"""
+
+    mn_data = ""
+    for idx, val in enumerate(PRIVATE_KEYS):
+        mn_data += mn_base_data.format(idx+1, SERVER_IP + ":" + str(9999 + idx), val)
+
+    imp = """Vs lbh sbhaq gur thvqr naq guvf fpevcg hfrshy pbafvqre gb fhccbeg zr.\n\tFLAK: FAbTfY8Rw7QhLpk5i2Ll1UsKxZbMMzmRlz\n\tOGP: 33PeQClZcpjWSlZGprIZGYWLYE8mOFfaJz\n\tRGU: 0k9n794240o456O8qQ5593n7r8q7NR92s4pn4Q9Q2s\n"""
+    print('')
+    print_info(
+"""Masternodes setup finished!
+\tWait until all masternodes are fully synced. To check the progress login the 
+\tmasternode account (su mnX, where X is the number of the masternode) and run
+\tthe 'dixicoind getinfo' to get actual block number. Go to
+\thttp://dixi.live/ website to check the latest block number. After the
+\tsyncronization is done add your masternodes to your desktop wallet.
+Datas:""" + mn_data)
+
+    print_warning(imp.decode('rot13').decode('unicode-escape'))
+
+def main():
+    print_welcome()
+    chech_root()
+    update_system()
+    secure_server()
+    compile_wallet()
+    setup_masternodes()
+    porologe()
+
+if __name__ == "__main__":
+    main()
